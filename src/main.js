d122e28db1738dd4ecb7c5bb1f0f1bcafc76546a
droid4j <android4j@163.com>
2020-11-14 18:03:43 +0800

    postcss
    这里我们设置了px和rem自动转换，我们用到了。
    1、自动设置rem 基准值，就是HTML标签字体大小，这个是在html标签上style上看的，切换手机型号可以看到变化
    import 'amfe-flexible'
    2、vuecli集成了浏览器兼容性
    # browserslist
    3、我们设置了vuecli自动生成的浏览器兼容配置文件/Users/per4j/learn/learn_vue/toutiao-m/.browserslistrc
    4、我们创了postcss配置文件
    /Users/per4j/learn/learn_vue/toutiao-m/postcss.config.js

--- src/main.js
+++ src/main.js
@@ -2,9 +2,15 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
-
+// 导入vant 组件库ui
+import Vant from 'vant'
+// 导入vant组件库的样式
+import 'vant/lib/index.css'
+// 自动设置rem 基准值，就是HTML标签字体大小，这个是在html标签上style上看的，切换手机型号可以看到变化
+import 'amfe-flexible'
+// 注册全局vant中的组件
+Vue.use(Vant)
 Vue.config.productionTip = false
-
 new Vue({
   router,
   store,
