493221f433ca32e46dacc3127bd94a6b9afca375
droid4j <android4j@163.com>
2020-11-14 21:47:31 +0800

    iconfont  导入字体图标
    这个用的是阿里开发的，vant也有自己的字体图标

--- src/App.vue
+++ src/App.vue
@@ -7,6 +7,9 @@
     <van-button type="default">默认按钮</van-button>
     <van-button type="warning">警告按钮</van-button>
     <van-button type="danger">危险按钮</van-button>
+    <div style="pading: 30px" class="box" >
+     hello
+    </div>
   </div>
 </template>
 <script>
