4bb3b35592f6aedff25b50ad66d51ab25d375710
droid4j <android4j@163.com>
2020-11-15 18:31:39 +0800

    登录布局完成
    1、带有图标的输入框（field如何换成自己的图标，用到的是icon-prefix 属性）

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -2,6 +2,7 @@
     <div class="login-container">
       <!-- 导航栏 navbar组件-->
       <van-nav-bar
+        class="nav-bar-app"
         title="登录"
         left-arrow
         @click-left="$router.back()"
@@ -11,23 +12,31 @@
       <van-cell-group>
         <van-field
           v-model="user.mobile"
-          left-icon="smile-o"
+          icon-prefix="toutiao"
+          left-icon="shouji"
           placeholder="输入用户名"
         />
         <van-field
           v-model="user.code"
           clearable
-          left-icon="music-o"
+          icon-prefix="toutiao"
+          left-icon="yanzhengma"
           placeholder="输入验证码"
         >
           <template #button>
             <van-button
+              class="send-btn"
               size="small"
               round
-            >发送验证码</van-button>
+            >获取验证码</van-button>
           </template>
         </van-field>
       </van-cell-group>
+      <!-- block块级属性，按钮横向填充满-->
+      <div class="login-btn-wrap">
+        <van-button  class="login-btn" type="info" block >登录</van-button>
+      </div>
+
       <!-- 登录表单-->
     </div>
 </template>
@@ -55,4 +64,27 @@ export default {
 </script>
 
 <style scoped lang="less">
+  .login-container{
+    .send-btn{
+      width: 80px;
+      height: 23px;
+      background-color: #ededed;
+      .van-button__text{
+        font-size: 11px;
+        color: #666666;
+      }
+    }
+    .login-btn-wrap{
+      padding: 26px 16px;
+      .login-btn{
+        background-color: #6db4fb;
+        border: none;
+        .van-button__text{
+          font-size: 15px;
+        }
+      }
+    }
+
+  }
+
 </style>
