623a0db0ee052c40e1e94dcb3681f5983c822089
droid4j <android4j@163.com>
2020-11-15 17:00:44 +0800

    创建登录页布局
    创建登录页用到的vant组件
    1、navbar(上边的导航栏)
    2、field(登录的输入框)
    3、button(登录按钮)

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -1,5 +1,35 @@
 <template>
-    <div class="login-container">登录界面</div>
+    <div class="login-container">
+      <!-- 导航栏 navbar组件-->
+      <van-nav-bar
+        title="登录"
+        left-arrow
+        @click-left="$router.back()"
+      />
+      <!-- 导航栏-->
+      <!-- 登录表单 field组件-->
+      <van-cell-group>
+        <van-field
+          v-model="user.mobile"
+          left-icon="smile-o"
+          placeholder="输入用户名"
+        />
+        <van-field
+          v-model="user.code"
+          clearable
+          left-icon="music-o"
+          placeholder="输入验证码"
+        >
+          <template #button>
+            <van-button
+              size="small"
+              round
+            >发送验证码</van-button>
+          </template>
+        </van-field>
+      </van-cell-group>
+      <!-- 登录表单-->
+    </div>
 </template>
 <script>
 export default {
@@ -7,7 +37,12 @@ export default {
   components: {},
   props: {},
   data () {
-      return {}
+      return {
+          user: {
+              mobile: '', // 手机号
+              code: '' // 验证码
+          }
+      }
   },
   computed: {},
   watch: {},
