8d2608b169075a471baeb74ee3a4a8a77c48fd7c
droid4j <android4j@163.com>
2020-11-24 17:36:51 +0800

    顶部搜索按钮

--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -1,5 +1,18 @@
 <template>
-    <div class="home-container">首页</div>
+    <div class="home-container">
+      <van-nav-bar
+        class="nav-bar-app"
+      >
+        <van-button
+          slot="title"
+          round
+          icon="search"
+          type="info"
+          size="small"
+          class="search-btn"
+        >搜索</van-button>
+      </van-nav-bar>
+    </div>
 </template>
 <script>
     export default {
@@ -20,4 +33,21 @@
 </script>
 
 <style scoped lang="less">
+  .home-container{
+    /deep/.van-nav-bar__title{
+      max-width: none;
+    }
+    .search-btn{
+      width: 277px;
+      height: 32px;
+      background: #5babfb;
+      border:none;
+      .van-icon{
+        font-size: 16px;
+      }
+      .van-button__text{
+        font-size: 14px;
+      }
+    }
+  }
 </style>
