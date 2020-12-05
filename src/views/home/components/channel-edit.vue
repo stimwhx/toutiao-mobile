90bccb18fb24d70c0028e5ce77abb53be52239f9
droid4j <android4j@163.com>
2020-12-05 11:28:45 +0800

    删除频道的处里。splice删除数组的方法

--- src/views/home/components/channel-edit.vue
+++ src/views/home/components/channel-edit.vue
@@ -7,7 +7,8 @@
           plain
           type="danger"
           size="mini"
-        >编辑</van-button>
+          @click="isEdit = !isEdit"
+        >{{ isEdit ? '完成': '编辑' }}</van-button>
       </van-cell>
       <van-grid :gutter="10">
         <van-grid-item
@@ -15,6 +16,8 @@
           v-for ="(channel,index) in userChannels"
           :key="index"
           :text="channel.name"
+          :icon="(isEdit && index !== 0) ? 'clear' : ''"
+          @click="onUserChannelEdit(index)"
         />
       </van-grid>
       <!--allchannel-->
@@ -45,7 +48,8 @@
         },
         data () {
             return {
-              allChannels: [] //所有频道数据列表
+              allChannels: [], //所有频道数据列表
+              isEdit: false
             }
         },
         computed: {
@@ -77,6 +81,22 @@
          },
           onAdd (channel) {
              this.userChannels.push(channel)
+          },
+          onUserChannelEdit (index) {
+             if (this.isEdit && index !== 0) {
+                 // 编辑按钮变为完成，执行删除操作
+               this.deleteChannel(index)
+             }else{
+                 // 执行切换频道
+               this.switchChannel(index)
+             }
+          },
+          deleteChannel (index) {
+             // 删除数组中的元素，index表示从第几个开始包含此位置的元素，1表示删除几个。省略就是删除到最后
+             this.userChannels.splice(index, 1)
+          },
+          switchChannel (index) {
+             console.log(index)
           }
         }
     }
@@ -92,13 +112,21 @@
     .grid-item {
       width: 80px;
       height: 43px;
-      /deep/ .van-grid-item-content {
+      /deep/.van-grid-item__content {
         background-color: #f4f5f6;
-        .van-grid-item-text {
+        .van-grid-item__text {
           font-size: 14px;
-          color: #222
+          color: #222;
+          margin-top:0;
         }
       }
+    }
+    /deep/.van-grid-item__icon {
+      position: absolute;
+      right: -5px;
+      top: -5px;
+      font-size: 15px;
+      color: #ccc;
     }
   }
 
