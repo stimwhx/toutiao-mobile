223dbc950e692e62de591b0c36ee804916c76d98
droid4j <android4j@163.com>
2020-12-10 07:33:14 +0800

    github-markdown-css 文章详情样式+jsonbig

--- src/utile/request.js
+++ src/utile/request.js
@@ -4,9 +4,25 @@
 import axios from 'axios'
 // 在非组件中获取store必须通过这种方式，单独加载
 import store from '@/store/'
-
+import JSONbig from 'json-bigint'
 const request = axios.create({
-  baseURL: 'http://ttapi.research.itcast.cn' // 基础路径
+  baseURL: 'http://ttapi.research.itcast.cn', // 基础路径
+  transformResponse: [function (data) {
+    // 后端的返回数据可能不是JSON格式字符串
+    // 如果不是的话那么，JSON.parse调用就会报错
+    // 所以我们用try--catch来捕获异常，处理异常的发生
+    try {
+        // 如果转成功了，则直接把结果返回
+      return JSONbig.parse(data)
+    } catch (err) {
+      console.log('转换失败了', err)
+      // 如果转换失败了就进入到这里了
+      // 我们在这里把数据原封不动的直接返回给请求使用。有的返回的数据可能不是JSON的我们不用转就直接返回就可以了
+
+      return data
+  }
+  // axios 默认在内部使用JSON.parse(data)来转换处理原始数据
+  }]
 })
 // 请求拦截器
 // Add a request interceptor
--- src/views/article/index.vue
+++ src/views/article/index.vue
@@ -1,32 +1,112 @@
 <template>
-    <div class="article-container">文章详情</div>
+    <div class="article-container">
+      <!-- 导航栏 navbar组件-->
+      <van-nav-bar
+        class="nav-bar-app"
+        title="文章详情"
+        left-arrow
+        @click-left="$router.back()"
+      />
+      <!-- 导航栏-->
+      <h1 class="title">{{ article.title}}</h1>
+      <van-cell
+        class="user-info"
+      center
+      >
+        <div slot="title" class="name">{{ article.aut_name }}</div>
+        <van-image
+          class="avator"
+          slot="icon"
+          round
+          fit="cover"
+          :src="article.aut_photo" />
+        <div slot="label" class="pudate">{{ article.pubdate | relativeTime}}</div>
+        <van-button
+          class="follow-btn"
+          :type="article.is_followed ? 'default' : 'info' "
+          round
+          size="small"
+          :icon="article.is_followed ? '' : 'plus' "
+        >{{ article.is_followed ? '己关注' : '关注' }}</van-button>
+      </van-cell>
+      <div class="markdown-body"
+      v-html="article.content"
+      >
+
+      </div>
+    </div>
 </template>
 <script>
   // 在组件中动太获取路由参数的方法有两种
   // 方式一： this.$route.params.articleID
   // 方式二： props 传参，推荐
   // this.articleId
+  import './github-markdown.css'
+  import { getArticleById } from '@/api/article'
     export default {
         name: 'ArticleIndex',
         components: {},
         props: {
           articleId: {
-              type: String,
+              type: [Number, String, Object],
             required: true
           }
         },
         data () {
-            return {}
+            return {
+                article: {} // 文章详情
+            }
         },
         computed: {},
         watch: {},
         created () {
+            this.loadArticle()
         },
         mounted () {
         },
-        methods: {}
+        methods: {
+          async loadArticle () {
+              const { data } = await getArticleById(this.articleId)
+            this.article = data.data
+          }
+        }
     }
 </script>
 
 <style scoped lang="less">
+
+  .title{
+    font-size: 14px;
+    color: #3a3a3a;
+    padding: 15px;
+    background-color: #fff;
+    margin: 0px;
+  }
+  .user-info{
+    .avator{
+      width: 35px;
+      height: 35px;
+      margin-right: 8px
+    }
+    .name{
+      font-size: 12px;
+      color: #333333
+    }
+    .pudate{
+      font-size: 12px;
+      color: #b4b4b4
+    }
+    .follow-btn{
+      width: 85px;
+      height: 29px;
+    }
+  }
+  ul{
+    list-style: unset;
+  }
+  .markdown-body{
+    padding: 14px;
+    background-color: #fff;
+
+  }
 </style>
